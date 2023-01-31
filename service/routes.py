57935ce9d82732f8e99d22a2f7a5afe11c440739
abaggett12 <104461949+abaggett12@users.noreply.github.com>
2023-01-31 15:38:19 -0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -99,6 +100,7 @@ def get_accounts(account_id):
 
 # ... place you code here to UPDATE an account ...
 
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -118,6 +120,7 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
